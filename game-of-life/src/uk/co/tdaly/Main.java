6602187fdbfdbbcd59262e8c6ae8cbae2737472b
tomdaly <tomdaly@users.noreply.github.com>
2019-02-05 15:47:40 +0000

    Add input panes to main application

--- game-of-life/src/uk/co/tdaly/Main.java
+++ game-of-life/src/uk/co/tdaly/Main.java
@@ -1,9 +1,47 @@
 package uk.co.tdaly;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        GameOfLife game = new GameOfLife(10, 10);
+        boolean validInputs = false;
+        int dimension, randomCells;
+        dimension = randomCells = 0;
+        do {
+            String input = (String)JOptionPane.showInputDialog(null, "Welcome to the Game of Life\nEnter game dimensions (between 8 and 40):");
+            if (input != null && input.length() > 0) {
+                try {
+                    dimension = Integer.parseInt(input);
+                    if (!(dimension >= 8 && dimension <= 40)) {
+                        throw new NumberFormatException();
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,"Dimensions must be an integer between 8 and 40!");
+                    continue;
+                }
+                int maxCells = dimension * dimension;
+                input = (String)JOptionPane.showInputDialog(null, "Enter amount of live cells to be randomly placed (between 1 and " + maxCells + "):");
+                if (input != null && input.length() > 0) {
+                    try {
+                        randomCells = Integer.parseInt(input);
+                        if (!(randomCells >= 1 && randomCells <= maxCells)) {
+                            throw new NumberFormatException();
+                        }
+                        validInputs = true;
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(null, "Live cell amount must be an integer between 1 and " + maxCells + "!");
+                    }
+                } else {
+                    System.exit(1);
+                }
+            } else {
+                System.exit(1);
+            }
+        } while (!validInputs);
+
+        GameOfLife game = new GameOfLife(dimension, dimension);
+        game.addRandomCells(randomCells);
         GUI gui = new GUI(game);
     }
 }
